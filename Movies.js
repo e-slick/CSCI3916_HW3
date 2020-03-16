8cabbafadbd34e1b5808c834f1aeac37bbbed461
e-slick <33139655+e-slick@users.noreply.github.com>
2020-03-15 22:23:40 -0600

    Add files via upload

---
+++ Movies.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+//TODO: Review https://mongoosejs.com/docs/validation.html
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DB, { useNewUrlParser: true} );
+mongoose.set('useCreateIndex', true);
+
+// movie schema
+var MovieSchema = new Schema({
+    title: {type: String, required: true},
+    releaseDate: {type: String, required: true},
+    genre: {type: String, required: true},
+    actorName: {type: Array, required: true},
+    charName: {type: Array, required: true}
+});
